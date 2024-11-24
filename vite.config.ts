bf1b4fba244dd36554b5f898880ab07111a256bd
jellydn <jellydn@users.noreply.github.com>
2024-11-24 08:42:42 +0000

    Apply automatic changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
 import tailwindcss from "@tailwindcss/vite";
+import autoprefixer from "autoprefixer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
